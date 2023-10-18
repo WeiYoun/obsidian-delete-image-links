9ebf39d18dada6850997d99b92fbe92f7d608dec
Xm Guo <gxmhm@outlook.com>
2023-10-18 15:25:13 +0800

    add plugins

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # obsidian-delete-image-links
+## 介绍
 plugins to delete images in obsidian notes write with markdown.
+
+## 功能说明
+共有两个插件：
+### delete_all_notes_images
+该插件可以**删除所有**笔记中的外部图片，不影响本地图片。（**慎用**）
+
+### delete_current_note_images
+该插件可以删除当前笔记中的外部图片，不影响本地图片。
+
+## 使用说明
+将对应文件夹中的同名文件夹整个复制到obsidian的plugins文件夹下，然后再obsidian中安装插件即可使用。
+
+安装后再obsidian最左侧的图标面板会多出一个圆圈图标，点击后即可删除笔记中的图片。
+
+## 开发文档
+## [开发obsidian插件入门](https://www.weiyoun.com/2023/10/17/kai-fa-obsidian-cha-jian-ru-men/)
+## [开发obsidian插件——删除所有笔记中的图片](https://www.weiyoun.com/2023/10/17/kai-fa-obsidian-cha-jian-shan-chu-suo-you-bi-ji-zhong-de-tu/)
+## [开发obsidian插件——删除当前笔记中的图片](https://www.weiyoun.com/2023/10/17/kai-fa-obsidian-cha-jian-shan-chu-dang-qian-bi-ji-zhong-de/)
+
+## 开发交流
+![](https://easyimage.weiyoun.com/i/2023/09/08/tyvz4w-3.png)
---
+++ delete_current_note_images/src/main.ts
@@ -0,0 +1,128 @@
+import {
+  App,
+  Modal,
+  Notice,
+  Plugin,
+  PluginSettingTab,
+  Setting,
+} from "obsidian";
+interface DeleteImageLinksSettings {
+  mySetting: string;
+}
+const DEFAULT_SETTINGS: DeleteImageLinksSettings = {
+  mySetting: "default",
+};
+export default class DeleteImageLinks extends Plugin {
+  settings: DeleteImageLinksSettings;
+  async onload() {
+    console.log("loading plugin");
+    await this.loadSettings();
+    this.addRibbonIcon("circle", "delete images", () => {
+      // new Notice("This is a notice!");
+      this.deleteImageLinks();
+    });
+    this.addStatusBarItem().setText("Status Bar Text");
+    this.addCommand({
+      id: "open-sample-modal",
+      name: "Open Sample Modal",
+      // callback: () => {
+      // 	console.log('Simple Callback');
+      // },
+      checkCallback: (checking: boolean) => {
+        let leaf = this.app.workspace.activeLeaf;
+        if (leaf) {
+          if (!checking) {
+            new SampleModal(this.app).open();
+          }
+          return true;
+        }
+        return false;
+      },
+    });
+    this.addSettingTab(new SampleSettingTab(this.app, this));
+    this.registerCodeMirror((cm: CodeMirror.Editor) => {
+      console.log("codemirror", cm);
+    });
+    this.registerDomEvent(document, "click", (evt: MouseEvent) => {
+      console.log("click", evt);
+    });
+    this.registerInterval(
+      window.setInterval(() => console.log("setInterval"), 5 * 60 * 1000)
+    );
+  }
+  onunload() {
+    console.log("unloading plugin");
+  }
+  async loadSettings() {
+    this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+  }
+  async saveSettings() {
+    await this.saveData(this.settings);
+  }
+
+
+  async deleteImageLinks() {
+
+      const note = this.app.workspace.getActiveFile();
+      const content = this.app.vault.read(note);
+  
+      try {
+        const resolvedString: string = await content; // 等待 Promise 解析为字符串
+        const modifiedString: string = resolvedString.replace(/!\[[^\]]*\]\([^)]*\)/g, ''); // 使用 replace 方法对字符串进行修改
+        console.log(modifiedString);
+        this.app.vault.modify(note, modifiedString);
+      } catch (error) {
+        console.error(error);
+      }
+
+    // 提示删除成功
+    new Notice('All image links have been deleted from your notes.');
+  }
+  
+}
+
+
+
+class SampleModal extends Modal {
+  constructor(app: App) {
+    super(app);
+  }
+  onOpen() {
+    let { contentEl } = this;
+    contentEl.setText("Woah!");
+  }
+  onClose() {
+    let { contentEl } = this;
+    contentEl.empty();
+  }
+}
+
+class SampleSettingTab extends PluginSettingTab {
+  plugin: DeleteImageLinks;
+  constructor(app: App, plugin: DeleteImageLinks) {
+    super(app, plugin);
+    this.plugin = plugin;
+  }
+  display(): void {
+    let { containerEl } = this;
+    containerEl.empty();
+    containerEl.createEl("h2", { text: "Settings for my awesome plugin." });
+    new Setting(containerEl)
+      .setName("Setting #1")
+      .setDesc("It's a secret")
+      .addText((text) =>
+        text
+          .setPlaceholder("Enter your secret")
+          .setValue("")
+          .onChange(async (value) => {
+            console.log("Secret: " + value);
+            this.plugin.settings.mySetting = value;
+            await this.plugin.saveSettings();
+          })
+      );
+  }
+}
+
+
+
+
